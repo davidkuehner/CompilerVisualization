e90b23038479f0af7fda7b73f0eb98d78b7b1ea5
davidkuhner <dav.kuhn@gmail.com>
2013-10-25 12:57:41 +0200

    Add files for Class svg output

---
+++ GraphicTools/Line.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Hoathis\GraphicTools {
+	
+require_once __DIR__.'/Leaf.php';
+
+/**
+ * Class \Hoathis\GraphicTools\Line.
+ *
+ * Concrete class that represents a svg line and 
+ * primitives element for Composite pattern.
+ *
+ * @author     David Kühner <david.kuhner@he-arc.ch>
+ * @copyright  Copyright © 2007-2013 David Kühner
+ * @license    New BSD License
+ */
+
+class Line  extends \Hoathis\GraphicTools\Leaf {
+
+	/**
+     * Main constructor
+     */
+	function __construct() {
+
+        $this->attributes = array();
+        $this->elements = array();
+        
+        $this->setAttributes( array( 'style'=>'stroke:rgb(255,0,0);stroke-width:2' ));
+    }
+	
+}
+
+}
