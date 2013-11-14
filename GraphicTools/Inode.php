d2b1f6d232549c1b507ef12a7238ea27fe38be44
davidkuhner <dav.kuhn@gmail.com>
2013-11-14 10:58:04 +0100

    Add Wrapping, Horizontal and Vertical Layout. Simple Loop with token label

--- GraphicTools/Inode.php
+++ GraphicTools/Inode.php
@@ -23,28 +23,89 @@ namespace Hoathis\GraphicTools {
  * @license    New BSD License
  */
 
-abstract class Inode extends Graphic implements \Hoathis\Regex\Visitor\Buildable {
+abstract class Inode extends Graphic {
 	
 	/**
 	 * List of elements in the group 
 	 */
-	private $elements;
+	protected $elements;
+	
+	/**
+	 * Margin between the elements in the group 
+	 */
+	private $margin;
+	
+	private $isLayoutVertical;
+	private $isLayoutHorizontal;
+	private $isLayoutWrapping;
+	private $hasPath;
+	private $hasBypass;
+	private $hasLoop;
+	private $hasLoopLabel;
+	private $loopLabel;
 	
 	/**
      * Main constructor
      */
 	function __construct() {
 		parent::__construct();
+		
+		// Default initialisation
 		$this->elements = array();
+		$this->isLayoutVertical = false;
+		$this->isLayoutHorizontal  = false;
+		$this->isLayoutWrapping = false;
+		$this->hasPath  = false;
+		$this->hasBypass  = false;
+		$this->hasLoop  = false;
+		$this->hasLoopLabel  = false;
+		$this->loopLabel = '';
+		$this->margin = 0;
 	}
 	
+	
+	
 	/**
-     * Add a child svg element.
+     * Adds a child of type buildable to the buildable element.
+     *
+     * @access  public
+     */
+    public function addChild( \Hoathis\Regex\Visitor\Buildable $child) {
+		
+		if( $this->isLayoutVertical ) {
+			
+			$this->setHeight( $this->getHeight() + $child->getHeight() + $this->margin, SvgCreator::UNITS );
+			if ( $this->getWidth() < $child->getWidth() ) {
+				$this->setWidth( $child->getWidth() + $this->margin * 2, SvgCreator::UNITS);
+			}
+			
+		} else if( $this->isLayoutHorizontal ) {
+			
+			if ( $this->getHeight() < $child->getHeight() ) {
+				$this->setHeight( $child->getHeight() + $this->margin * 2, SvgCreator::UNITS);
+			}
+			$this->setWidth( $this->getWidth() + $child->getWidth() + $this->margin, SvgCreator::UNITS );
+			
+		} else if( $this->isLayoutWrapping ) {
+
+			if ( $this->getHeight() < $child->getHeight() ) {
+				$this->setHeight( $child->getHeight() + $this->margin * 2, SvgCreator::UNITS);
+			}
+			if ( $this->getWidth() < $child->getWidth() ) {
+				$this->setWidth( $child->getWidth() + $this->margin * 2, SvgCreator::UNITS);
+			}
+			
+		}
+		$this->elements[] = $child;
+	}
+		
+	/**
+     * Adds a child to the svg element without layout context.
      *
      * @access  public
      * @param   \Hoathis\GraphicTools\Graphic	$element     The child element to add.
      */
-    public function addElement( Graphic $element ) {
+    private function addElement( Graphic $element ) {
 		$this->elements[] = $element;
 	}
 	
@@ -68,9 +129,47 @@ abstract class Inode extends Graphic implements \Hoathis\Regex\Visitor\Buildable
 		 		
 		// Finds the class name without namespace
 		$className = strtolower( get_class( $this ) );
+		// Finds the class name without namespace
+		$className = strtolower( get_class( $this ) );
+		// Gets the name without the namespace
 		preg_match( '/\w+$/', $className, $match );
 		$className = $match[0];
 		
+		
+		$builder = $this->buildOpenerTag( $className );
+		
+		
+		// Sets (x;y) position to the child according to the layout
+		// And adds paths to each children if asked
+		if( $this->isLayoutVertical ) {
+			$this->buildVertivalLayout();
+		}
+		else if( $this->isLayoutHorizontal ) {
+			$this->buildHorizontalLayout();
+		}
+		
+		
+		// Construct a loop, can be bypass or loop. It's defined by
+		// the token type of the second child.
+		if( $this->hasLoop ) {
+			$this->buildLoop();
+		}
+		
+		
+		// Adds children elements
+		foreach ( (array)$this->elements as $element ) {
+				$builder .= $element->build();
+			}
+			
+			
+		$builder .= $this->buildCloserTag( $className );
+		//$builder .= "<!-- debug: " . $className. " -->";
+		
+		return $builder;
+	 }
+	 
+	 private function buildOpenerTag( $className ) {
+
 		$builder = '<' . $className;
 
 		// Adds every attributes in the tag
@@ -79,17 +178,95 @@ abstract class Inode extends Graphic implements \Hoathis\Regex\Visitor\Buildable
 		}
 		$builder .= '>';
 		
-		// Adds children elements
-		foreach ( (array)$this->elements as $element ) {
-				$builder .= $element->build();
-			}
-		
-		// Close the current element with the closer tag
-		$builder .= '</' . $className . '>';
 		return $builder;
 	 }
 	 
+	 private function buildCloserTag( $className ) {
+		 // Close the current element with the closer tag
+		return '</' . $className . '>';
+	 }
 	 
+	 private function buildVertivalLayout() {
+		 $childIndex = 0;
+
+		// Distribute the token along the height
+		foreach( $this->getElements() as $element ) {
+			$childHeight = $element->getHeight();
+			$childYPos = $childIndex*$childHeight + $childIndex*$this->margin;
+			$u = SvgCreator::UNITS; // units
+			
+					
+			// Adds path if nessesary
+			if( $this->hasPath ) {
+				$pathYchild = $childYPos + $childHeight / 2;
+				$outY = $this->getHeight() / 2;
+
+				$pathIn = new \Hoathis\GraphicTools\Line();
+				$pathOut = new \Hoathis\GraphicTools\Line();
+				$pathIn->setAttributes( array( 'x1'=>'0', 'y1'=>$outY.$u, 'x2'=>$this->margin.$u, 'y2'=>$pathYchild.$u ) );
+				$pathOut->setAttributes( array( 'x1'=>( $this->getWidth() - $this->margin).$u, 'y1'=>$pathYchild.$u, 'x2'=>$this->getWidth().$u, 'y2'=>$outY.$u ) );
+				$this->addElement( $pathIn );
+				$this->addElement( $pathOut );
+			}
+			
+			$element->setAttributes( array( 'y'=> $childYPos . $u, 'x'=> $this->margin.$u ) );
+			
+			++$childIndex;
+		}
+	 }
+	 
+	 private function buildHorizontalLayout() {
+		$childIndex = 0;
+
+		// Distribute the token along the height
+		foreach( $this->getElements() as $element ) {
+			$childWidth = $element->getWidth();
+			$childXPos = $childIndex*$childWidth + $childIndex*$this->margin;
+			$u = SvgCreator::UNITS; // units
+			
+			$element->setAttributes( array( 'y'=> $this->getHeight()/2 - $element->getHeight()/2 . $u, 'x'=> $childXPos . $u ) );
+			
+			++$childIndex;
+		}
+	 }
+	 
+	 private function buildLoop() {
+		$child = $this->elements[0]; // first element of the quantification is the core
+		$label = $this->elements[1]; // second element is the token that define the quantification
+
+		$u = SvgCreator::UNITS; // units
+				
+		$a = array('x' => 0, 'y' => $child->getHeight()/2 + $this->margin);
+		$b = array('x' => $this->margin, 'y' => $a['y']);
+		$c = array('x' => $b['x'], 'y' => 0);
+		$d = array('x' => $child->getWidth() + $this->margin, 'y' => 0);
+		$e = array('x' => $d['x'], 'y' => $a['y']);
+		$f = array('x' => $e['x'] + $this->margin, 'y' => $a['y']);
+		
+		$ab = new \Hoathis\GraphicTools\Line();
+		$ab->setAttributes( array( 'x1'=>$a['x'].$u, 'y1'=>$a['y'].$u, 'x2'=>$b['x'].$u, 'y2'=>$b['y'].$u ) );
+		$this->addElement( $ab );
+		
+		$bc = new \Hoathis\GraphicTools\Line();
+		$bc->setAttributes( array( 'x1'=>$b['x'].$u, 'y1'=>$b['y'].$u, 'x2'=>$c['x'].$u, 'y2'=>$c['y'].$u ) );
+		$this->addElement( $bc );
+		
+		$cd = new \Hoathis\GraphicTools\Line();
+		$cd->setAttributes( array( 'x1'=>$c['x'].$u, 'y1'=>$c['y'].$u, 'x2'=>$d['x'].$u, 'y2'=>$d['y'].$u ) );
+		$this->addElement( $cd );
+		
+		$de = new \Hoathis\GraphicTools\Line();
+		$de->setAttributes( array( 'x1'=>$d['x'].$u, 'y1'=>$d['y'].$u, 'x2'=>$e['x'].$u, 'y2'=>$e['y'].$u ) );
+		$this->addElement( $de );
+		
+		$ef = new \Hoathis\GraphicTools\Line();
+		$ef->setAttributes( array( 'x1'=>$e['x'].$u, 'y1'=>$e['y'].$u, 'x2'=>$f['x'].$u, 'y2'=>$f['y'].$u ) );
+		$this->addElement( $ef );
+		
+		$child->setAttributes( array( 'x'=>$this->margin.$u, 'y'=>$this->margin.$u ));
+		$label->setAttributes( array( 'x'=>$this->margin*2+$child->getWidth() . $u, 'y'=>$this->margin.$u ));
+		
+	 }
 	 
 	 
 	 
@@ -98,37 +275,69 @@ abstract class Inode extends Graphic implements \Hoathis\Regex\Visitor\Buildable
      *
      * @access  public
      */
-    public function addPaths ( ) {}
+    public function addPaths ( ) {
+			$this->hasPath = true;
+		}
     
     /**
      * Adds a bypass to the buildable element
      *
      * @access  public
      */
-    public function addByPath ( ) {}
+    public function addBypass ( ) {
+			$this->hasBypass = true;
+		}
     
     /**
      * Adds a loop over the buildable element.
      *
      * @access  public
      */
-    public function addLoop ( ) {}
+    public function addLoop ( ) {
+			$this->hasLoop = true;
+		}
     
     /**
      * Adds a label to the loop.
      *
      * @access  private
      */
-    public function addLoopLabel ( ) {}
+    public function addLoopLabel ( $label ) {
+			$this->hasLoopLabel = true;
+			$this->loopLabel = $label;
+		}
     
     /**
-     * Adds a child of type buildable to the buildable element.
-     *
-     * @access  public
+     *	Sets the margin attribute 
      */
-    public function addChild( \Hoathis\Regex\Visitor\Buildable $child) {}
+	 public function setMargin( $margin ) {
+		 $this->margin = $margin;
+	 }
 	 
-	
+	 /**
+	  * Sets the layout orientation to vertical
+	  */
+	public function setVerticalLayout( ) {
+		$this->isLayoutVertical = true;
+		$this->isLayoutHorizontal = false;
+		$this->isLayoutWrapping = false;
+	}
+	 /**
+	  * Sets the layout orientation to horizontal
+	  */
+	public function setHorizontalLayout( ) {
+		$this->isLayoutVertical = false;
+		$this->isLayoutHorizontal = true;
+		$this->isLayoutWrapping = false;
+	}
+	/**
+	  * Sets the layout to wrapping content
+	  */
+	public function setWrappingLayout( ) {
+		$this->isLayoutVertical = false;
+		$this->isLayoutHorizontal = false;
+		$this->isLayoutWrapping = true;
+	}
 }
 
 }
