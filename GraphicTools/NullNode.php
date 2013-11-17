8cd6c9bdc33342a539147fbd396886931e3f9795
davidkuhner <dav.kuhn@gmail.com>
2013-11-17 15:03:49 +0100

    Add a null node for none representable elements

---
+++ GraphicTools/NullNode.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Hoathis\GraphicTools {
+	
+require_once __DIR__.'/Graphic.php';
+
+
+/**
+ * Class \Hoathis\GraphicTools\NullNode.
+ *
+ * Concret class that represent an element without representation to build.
+ * It's based on the Null Object design pattern.
+ *
+ * @author     David Kühner <david.kuhner@he-arc.ch>
+ * @copyright  Copyright © 2007-2013 David Kühner
+ * @license    New BSD License
+ */
+
+class NullNode extends \Hoathis\GraphicTools\Graphic {
+	
+	/**
+     * Build nothing.
+     *
+     * @access  public
+     * @return  string
+     */
+     public function build () {
+		 return '';
+	 }
+	
+}
+
+}
